61d1bdfc5a965e76d4dbb9d094488e12a67e1df5
Paul Mullins <paulmullins@protonmail.com>
2018-04-21 17:46:43 -0400

    Saving more work done on Identity.sol

--- truffleWorkspace/contracts/Identity.sol
+++ truffleWorkspace/contracts/Identity.sol
@@ -8,8 +8,12 @@ contract Identity {
         uint16 validity;
         bool ownerAccepts;
     }
+    struct identityAddress {
+        address accountAddress;
+        bool valid;
+    }
     mapping (bytes32 => bool) validIdentityKeys;
-    mapping (bytes32 => address[]) public identities;
+    mapping (bytes32 => identityAddress[]) public identities;
     mapping (bytes32 => mapping(bytes32 => identityToken)) identityTokens;
     //Functions
 
@@ -17,24 +21,37 @@ contract Identity {
     //Constructor
     //Curently I have nothing for it to do
     //This is a decentralized identity platform, so I have no authory anyways
+    address creator;
     function Identity () internal {
-
+        creator = msg.sender;
     }
 
 
     function createIdentity (bytes32 _identityKey) public returns (bool) {
         if(validIdentityKeys[_identityKey] == false){
             validIdentityKeys[_identityKey] = true;
-            identities[_identityKey].push(msg.sender);
+            identities[_identityKey].push(identityAddress(msg.sender, true));
             return(true);
         }
         else{
             return(false);
         }
     }
-    function addPublicKey () public {}
-    function deletePublicKey () public {}
-    function addPGPKey () public {}
+    function addPublicKey (bytes32 _userID, uint16 _position, address newPublicKey) public returns(bool) {
+        if(identities[_userID][_position].accountAddress == msg.sender){
+            identities[_userID].push(identityAddress(newPublicKey, true));
+            return(true);
+        }
+        else {
+            return(false);
+        }
+    }
+    function deletePublicKey () public {
+        //How do we remove keys from the middle of the array
+    }
+    function addPGPKey (string _key) public {
+        //Doing this next
+    }
     function addRawPGPKey () public {}
     function removePGPKey () public {}
     function selectCurrentPGPKey () public {}
