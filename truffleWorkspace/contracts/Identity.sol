d80efad339da4cb20e5cccdf867a5e8dda8b76e8
Paul Mullins <paulmullins@protonmail.com>
2018-04-21 16:19:45 -0400

    Working on the functions

--- truffleWorkspace/contracts/Identity.sol
+++ truffleWorkspace/contracts/Identity.sol
@@ -1,8 +1,37 @@
 pragma solidity ^0.4.16;
 
 contract Identity {
-    function identity () internal {}
-    function createIdentity () public {}
+    //Datastructures
+    struct identityToken {
+        bytes32 verifiedHASH;
+        bytes32 encryptedIPFSLink;
+        uint16 validity;
+        bool ownerAccepts;
+    }
+    mapping (bytes32 => bool) validIdentityKeys;
+    mapping (bytes32 => address[]) public identities;
+    mapping (bytes32 => mapping(bytes32 => identityToken)) identityTokens;
+    //Functions
+
+
+    //Constructor
+    //Curently I have nothing for it to do
+    //This is a decentralized identity platform, so I have no authory anyways
+    function Identity () internal {
+
+    }
+
+
+    function createIdentity (bytes32 _identityKey) public returns (bool) {
+        if(validIdentityKeys[_identityKey] == false){
+            validIdentityKeys[_identityKey] = true;
+            identities[_identityKey].push(msg.sender);
+            return(true);
+        }
+        else{
+            return(false);
+        }
+    }
     function addPublicKey () public {}
     function deletePublicKey () public {}
     function addPGPKey () public {}
