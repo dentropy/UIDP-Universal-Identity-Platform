11c802e29137a8c19615dc59d409a87da981ad27
Paul Mullins <paulmullins@protonmail.com>
2018-04-21 14:00:53 -0400

    Created an outline of the identity contract

---
+++ truffleWorkspace/contracts/Identity.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.16;
+
+contract Identity {
+    function identity () internal {}
+    function createIdentity () public {}
+    function addPublicKey () public {}
+    function deletePublicKey () public {}
+    function addPGPKey () public {}
+    function addRawPGPKey () public {}
+    function removePGPKey () public {}
+    function selectCurrentPGPKey () public {}
+    function addTokenMetaData () public {}
+    function appendTokenMetadata () public {}
+    function issueToken () public {}
+    function revokeToken () public {}
+
+}
