237af01351f6985eff5d7da82904dfd8ecd19b48
Paul Mullins <paulmullins@protonmail.com>
2018-04-22 13:21:07 -0400

    Deploy to localhost

--- truffleWorkspace/truffle.js
+++ truffleWorkspace/truffle.js
@@ -1,10 +1,11 @@
 module.exports = {
   networks: {
-    /*development: {
+    development: {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
     }
+    /*
     rinkeby: {
       host: "https://rinkeby.infura.io/",
       port: 8545,
@@ -12,9 +13,9 @@ module.exports = {
     }
     */
 
-    development: {
+    /*development: {
       provider: new HDWalletProvider("conduct coil cactus atom need donate paddle fossil moon impact cargo text", "https://ropsten.infura.io/"),
       network_id: '3',
-    },
+    },*/
   }
 };
