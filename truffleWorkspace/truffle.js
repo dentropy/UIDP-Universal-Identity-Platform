c8ba5c6736c44958c7d6a618102f4e03328af87d
Paul Mullins <paulmullins@protonmail.com>
2018-04-22 12:41:16 -0400

    Trying to get truffle to put stuff on a testnet

--- truffleWorkspace/truffle.js
+++ truffleWorkspace/truffle.js
@@ -1,9 +1,20 @@
 module.exports = {
   networks: {
-    development: {
+    /*development: {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
     }
+    rinkeby: {
+      host: "https://rinkeby.infura.io/",
+      port: 8545,
+      network_id: "*" // Match any network id
+    }
+    */
+
+    development: {
+      provider: new HDWalletProvider("conduct coil cactus atom need donate paddle fossil moon impact cargo text", "https://ropsten.infura.io/"),
+      network_id: '3',
+    },
   }
 };
