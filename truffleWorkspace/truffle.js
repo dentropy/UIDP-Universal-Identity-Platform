5e4a3b2a3964e1dbffa21324b5e159e9cdbebf24
Your Name <you@example.com>
2018-04-23 10:32:36 -0400

    Contracts now deployed to ringby

--- truffleWorkspace/truffle.js
+++ truffleWorkspace/truffle.js
@@ -1,11 +1,14 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
 module.exports = {
   networks: {
+    /*
     development: {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
     }
-    /*
+    
     rinkeby: {
       host: "https://rinkeby.infura.io/",
       port: 8545,
@@ -13,9 +16,9 @@ module.exports = {
     }
     */
 
-    /*development: {
+    development: {
       provider: new HDWalletProvider("conduct coil cactus atom need donate paddle fossil moon impact cargo text", "https://ropsten.infura.io/"),
       network_id: '3',
-    },*/
+    },
   }
 };
